aae1a5745a9472692de36932498bca0f0c7fbe6e
Emiyo0526 <118792684+Emiyo0526@users.noreply.github.com>
2023-02-08 15:00:40 +0900

    Update tunagi.js
    Signed-off-by: Emiyo0526 <118792684+Emiyo0526@users.noreply.github.com>

--- tunagi.js
+++ tunagi.js
@@ -9,8 +9,8 @@
  log シートに書き込む文字列
 ———————————–*/
 function addLog(log) {
-  const ss = SpreadsheetApp.openById("シートID");
-  const sheet = ss.getSheetByName("シート名");
+  const ss = SpreadsheetApp.openById("1icEJZ8Ht51WQxoAdHM6P3x-S0roqt97T0blklzW4_OU");
+  const sheet = ss.getSheetByName("シート1");
   sheet.appendRow([new Date(), log]);
   return log
 }
